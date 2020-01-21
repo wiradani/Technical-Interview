fb99bba204853c44869f5824c1610a9ed41a0026
Wiradani R <adhan432@gmail.com>
2020-01-21 21:45:48 +0700

    initial-create index and problem 1 page

---
+++ WebSolution/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Technical Interview - Solution</h1>
+
+<ul>
+  <li><a href="problem1.php">Problem 1</a></li>
+  <li><a href="problem2.php">Problem 2</a></li>
+  <li><a href="problem3.php">Problem 3</a></li>
+</ul>
+
+
+</body>
+</html>
+
---
+++ WebSolution/problem1.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Technical Interview - Solution</h1>
+<h2>Problem 1</h2>
+
+<form action="/action_page.php">
+  Input: <input type="text" name="input"><br>
+  <input type="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
+
