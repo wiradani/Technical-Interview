a4d42c4e9fc59f1eff987976d2cca3c1a0d7d75f
Wiradani R <adhan432@gmail.com>
2020-01-23 08:33:23 +0700

    add detail to view problem 2

--- WebSolution/view_problem2.php
+++ WebSolution/view_problem2.php
@@ -10,7 +10,7 @@
     require "problem.php";
 
        
-
+   $solution = "";
          
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $arr = $_POST["input"];
@@ -31,8 +31,18 @@
       ?>
     <h2>Technical Interview - Solution</h1>
     <h3>Problem 2</h2>
-      
-      
+
+    <p>
+    Terdapat papan catur yang ditempati oleh 8 buah bidak ratu secara acak. Pergerakan bidak ratu bisa mendatar ke kiri, kanan, atas, bawah, diagonal kanan kiri atas, maupun diagonal kanan kiri bawah.
+    Buatlah sebuah function findCrashPlace() yang berfungsi untuk mencari bidak mana saja yang akan memakan bidak lainnya dimulai dari POJOK KIRI BAWAH. Jika tidak ditemukan bidak ratu yang saling memakan maka berikan return string "tidak ditemukan". Bentuk input adalah sebuah array koordinat dengan susunan koordinat (baris,kolom)
+   </p>
+
+   <p>
+   Contoh input : array("(1,2)", "(2,5)", "(3,7)", "(4,4)", "(5,1)", "(6,8)", "(7,6)", "(8,3)")
+   </p>
+   
+   <img src="src/prob2Visual.png">
+
       <form method = "post" action = "view_problem2.php">
          <table>
             <tr>
